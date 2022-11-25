c254ed8b77ed13f260a2692ab7425c898e9f6482
bambademe <cheikhbambademe@gmail.com>
2022-11-25 00:08:06 +0000

    test table per class strategy

--- src/main/java/com/deme/ahmadou/ebank/entities/CurrentAccount.java
+++ src/main/java/com/deme/ahmadou/ebank/entities/CurrentAccount.java
@@ -8,7 +8,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("CA")
+//@DiscriminatorValue("CA")
 
 @Data @AllArgsConstructor @NoArgsConstructor
 public class CurrentAccount extends BankAccount{
